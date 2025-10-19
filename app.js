74039eba793fb730520b1b59471c9c816e492add
유제혁 <wpgur07@gmail.com>
2025-10-19 12:55:24 +0900

    feat: 404페이지 추가, index.js 제거
    - 잘못된 자원에 대한 접근을 처리하기 위해 404 페이지 추가
    - 404 status + 404.html 반환
    
    - indexRouter 제거, 페이지 로직을 app.js에 구현
    - 개발자가 정의한 페이지만 제공하도록 PAGES 변수로 관리
    - 또한 정의되지 않은 경로로 탐색시 404가 로드될 수 있도록 구현
    
    - 404 페이지에, 로그인, 게시물 목록 페이지로 갈 수 있도록 화면 구성
    
    - 추후 PAGES 변수를 관리하는 로직 추가 필요

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
-var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-
-var indexRouter = require("./routes/index");
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -17,16 +14,38 @@ app.use(cookieParser());
 // 정적 파일 루트
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
+// 정적파일 사전 검증 로직 필요
+const PAGES = [
+  "/login",
+  "/signin",
+  "/post",
+  "/post-create",
+  "/post-list",
+  "/404",
+];
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
+app.use(PAGES, (req, res) => {
+  const name = req.baseUrl.slice(1); // "/login" -> "login"
+  const file = path.join(__dirname, "public", "pages", name, `${name}.html`);
+  res.sendFile(file, (err) => {
+    if (err) {
+      res
+        .status(404)
+        .sendFile(path.join(__dirname, "public", "pages", "404", "404.html"));
+    }
+  });
+});
+
+// catch 404, PAGES로 관리되지 않는 경로 탐색 시 404
+app.use((req, res) => {
+  res
+    .status(404)
+    .sendFile(path.join(__dirname, "public", "pages", "404", "404.html"));
 });
 
 // 에러 핸들러: 텍스트/JSON로 처리
 app.use((err, req, res, next) => {
-  console.error(err);
+  console.error(req.url);
   res
     .status(err.status || 500)
     .json({ message: err.message || "Server Error" });

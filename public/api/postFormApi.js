1e0b5e157b4c9a638e63b22322afcf38d9315749
유제혁 <wpgur07@gmail.com>
2025-10-23 10:57:44 +0900

    refactor: 헤더 template에서  토큰 검증 과정 제거
    - 현재 인가정보가 필요한 페이지들 모두 api를 요청하게 되어 api에서 일관되게 처리할 수 있는 상황이 되었음
    - header를 삽입하는 common.js에서 토큰 유무를 삭제
    - apiClient에서 간단한 에러처리 진행
    - 500은 최하위 baseFetch에서 빈 에러 던짐.
    - 401은 토큰을 무조건 재발급 받아야하는 상황이라 상정하고 처리함
    - 그에 따라 중복된다고 판단되는 코드 삭제 postFromApi.js 401처리 구문 삭제 상위로 위임
    
    - 별도로 동작하지 않는 구문 동작하도록 변경 signin.js의 location.replace -> location.href 사용

--- public/api/postFormApi.js
+++ public/api/postFormApi.js
@@ -1,35 +1,18 @@
 import { authClient } from "./apiClient.js";
 
 export const createPost = async (body) => {
-  try {
-    const res = await authClient.post("/posts", body);
-    if (res.status === 204) {
-      alert("게시물이 등록되었습니다.");
-      window.location.href = `/post/${src}`;
-    }
-  } catch (err) {
-    if (err.status === 401) {
-      alert("로그인이 필요합니다.");
-      window.location.href = "/login";
-    } else {
-      alert("등록 실패: " + err.message);
-    }
+  const res = await authClient.post("/posts", body);
+  if (res.status === 204) {
+    const data = await res.json();
+    alert("게시물이 등록되었습니다.");
+    window.location.href = `/post/${data.data.postId}`;
   }
 };
 
 export const updatePost = async (src, body) => {
-  try {
-    const res = await authClient.put(`/posts/${src}`, body);
-    if (res.status === 204) {
-      alert("게시물이 수정되었습니다.");
-      window.location.href = `/post/${src}`;
-    }
-  } catch (err) {
-    if (err.status === 401) {
-      alert("로그인이 필요합니다.");
-      window.location.href = "/login";
-    } else {
-      alert("등록 실패: " + err.message);
-    }
+  const res = await authClient.put(`/posts/${src}`, body);
+  if (res.status === 204) {
+    alert("게시물이 수정되었습니다.");
+    window.location.href = `/post/${src}`;
   }
 };
--- public/semantics/common.js
+++ public/semantics/common.js
@@ -1,30 +1,30 @@
 import { logout } from "../api/loginApi.js";
 import { getAccessToken } from "../api/sessionStorage.js";
 
-const pages = {
-  NON_AUTH: ["/signin", "/login"],
-  AUTH: ["/post-list", "/post-create", "/post"],
-};
+// 혹시 몰라 남겨둠, 지금은 중복 체크하게 되어 지움.
+// const pages = {
+//   NON_AUTH: ["signin", "login"],
+//   AUTH: ["post-list", "post-create", "post"],
+// };
 
-const HOME = "/post-list";
-const LOGIN = "/login";
+// const HOME = "post-list";
+// const LOGIN = "login";
 
-(function () {
-  const at = getAccessToken(); // 콜백 함수로 이후 기능 구현 필요
+// (function () {
+//   const at = getAccessToken(); // 콜백 함수로 이후 기능 구현 필요
+//   const goto = (url, msg) => {
+//     if (msg) alert(msg);
+//     if (url === currentPath) return;
+//     alert("세션이 만료되어 홈으로 이동합니다.");
+//     window.location.href = `/${url}`;
+//   };
 
-  const goto = (url, msg) => {
-    if (msg) alert(msg);
-    if (url === currentPath) return;
-    window.location.href = url;
-  };
-
-  const currentPath = window.location.pathname.replace(/\/+$/, "");
-  const isAuth = pages.AUTH.includes(currentPath);
-  const isNon = pages.NON_AUTH.includes(currentPath);
-
-  if (isAuth && !at) goto(LOGIN);
-  else if (isNon && at) goto(HOME, "로그인 된 상태입니다. 홈으로 이동합니다.");
-})();
+//   const currentPath = window.location.pathname.split("/")[1];
+//   const isAuth = pages.AUTH.includes(currentPath);
+//   const isNon = pages.NON_AUTH.includes(currentPath);
+//   if (isAuth && !at) goto(LOGIN);
+//   else if (isNon && at) goto(HOME, "로그인 된 상태입니다. 홈으로 이동합니다.");
+// })();
 
 async function insertHeader() {
   const mount = document.getElementById("app-header");
@@ -40,6 +40,7 @@ async function insertHeader() {
 
   const token = getAccessToken();
   if (token) {
+    // 회원 정보 채우는 로직 추가.
     const logoutButton = document.getElementById("logout");
     const avatar = document.getElementById("h-avatar");
 

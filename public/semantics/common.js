3ac433081ec44c7093aec0bb1541c12ddcffbe88
유제혁 <wpgur07@gmail.com>
2025-10-24 14:22:07 +0900

    feat: 헤더 프로필 드롭다운 구현
    - header.html/css 에서 드롭다운 관련 태그/스타일 추가
    - common.js에서 header의 드롭다운 이벤트 조작 구현
    - 프로필 onclick시프로필의 aria속성을 조작하고,  드롭다운 display를 조작하도록 설정
    - 세션스토리지에 토큰 유무에 따른 프로필 diaplay 조작

--- public/semantics/common.js
+++ public/semantics/common.js
@@ -7,7 +7,7 @@ const pages = {
 };
 
 const HOME = "post-list";
-// const LOGIN = "login";
+const LOGIN = "login";
 
 (function () {
   const at = getAccessToken(); // 콜백 함수로 이후 기능 구현 필요
@@ -22,6 +22,8 @@ const HOME = "post-list";
   const isNon = pages.NON_AUTH.includes(currentPath);
   if (isNon && at) goto(HOME, "로그인 된 상태입니다. 홈으로 이동합니다.");
 })();
+let avatar;
+let dropdown;
 
 async function insertHeader() {
   const mount = document.getElementById("app-header");
@@ -35,18 +37,28 @@ async function insertHeader() {
   t.innerHTML = html.trim();
   mount.appendChild(t.content.firstElementChild);
 
+  const titleText = document.querySelector(".h-title");
   const token = getAccessToken();
+  avatar = document.getElementById("h-avatar");
   if (token) {
     // 회원 정보 채우는 로직 추가.
-    const logoutButton = document.getElementById("logout");
-    const avatar = document.getElementById("h-avatar");
+    dropdown = document.querySelector("#profileMenu");
 
-    logoutButton.disabled = false;
-    avatar.disabled = false;
-    logoutButton.addEventListener("click", (e) => {
-      logout();
-    });
+    const logoutBtn = document.querySelector("#menuLogout");
+    logoutBtn.addEventListener("click", logout);
+  } else {
+    avatar.hidden = true;
   }
+  titleText.addEventListener("click", (e) => {
+    window.location.href = getAccessToken() ? HOME : LOGIN;
+  });
 }
-
-document.addEventListener("DOMContentLoaded", () => insertHeader());
+document.addEventListener("DOMContentLoaded", async () => {
+  await insertHeader();
+  console.log(avatar);
+  avatar.addEventListener("click", (e) => {
+    dropdown.hidden = !dropdown.hidden;
+    const expended = e.target.attributes["aria-expanded"].value;
+    e.target.setAttribute("aria-expanded", String(!expended));
+  });
+});

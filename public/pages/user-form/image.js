827f5f61e79eb674ca19f89d6f7f5bc73f0c6015
jehyeok <wpgur07@gmail.com>
2025-11-28 14:46:47 +0900

    feat: 이미지를 섹션을 조작하는 동작을 클래스로 구현
    - 이미지 api핸들러를 받고, 이미지 관련 태그를 가지고 조작하는 코드뭉치
    - 이벤트를 달고, presignedUrl을 세팅하고, 서버에서 필요한 dto>를 가지고 있는 클래스

---
+++ public/pages/user-form/image.js
@@ -0,0 +1,86 @@
+import { ImageHandler } from "../../fetch/imageApi.js";
+
+export const emptyTemplate =
+  () => `  <label for="profile" class="profile-label">
+    <div class="profile-preview" id="preview"></div>
+    <span class="plus-icon" id="plus">+</span>
+  </label>
+  <input id="profile" name="profile" type="file" accept="image/*" hidden />
+  <button type="button" class="remove-btn" id="removeBtn" style="display:none;">삭제</button>
+  <p class="helper">프로필 사진을 선택하세요.</p> `;
+
+export class ProfileUploader {
+  constructor(root, { onImageChange } = {}) {
+    this.root = root;
+    this.input = root.querySelector("#profile");
+    this.preview = root.querySelector("#preview");
+    this.plus = root.querySelector("#plus");
+    this.helper = root.querySelector(".helper");
+    this.removeBtn = root.querySelector("#removeBtn");
+
+    this.imageId = null;
+    this.onImageChange = onImageChange;
+    this.imageHandler = new ImageHandler();
+    this.imageOrigianlName = "";
+    this.initEvents();
+  }
+
+  initEvents() {
+    this.input.addEventListener("change", (e) => this.renderImage(e));
+    this.removeBtn.addEventListener("click", () => this.renderEmpty());
+  }
+
+  async setUrl() {
+    this.imageId = await this.imageHandler.setUrl();
+  }
+
+  renderEmpty() {
+    if (this.blobUrl) {
+      URL.revokeObjectURL(this.blobUrl);
+      this.blobUrl = null;
+    }
+
+    this.preview.style.backgroundImage = "";
+    this.plus.style.display = "block";
+    this.helper.style.display = "block";
+    this.removeBtn.style.display = "none";
+    this.input.value = "";
+    this.imageId = null;
+    this.imageOrigianlName = "";
+
+    if (this.onImageChange) this.onImageChange(null);
+  }
+
+  async renderImage(e) {
+    const file = e.target.files?.[0];
+
+    if (!file || !file.type.startsWith("image/")) {
+      alert("이미지만 업로드 해주세요.");
+      this.renderEmpty();
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append("image", file);
+    this.imageOrigianlName = file.name;
+    this.imageHandler.setFile(formData);
+    await this.imageHandler.push();
+
+    if (this.blobUrl) URL.revokeObjectURL(this.blobUrl);
+    this.blobUrl = URL.createObjectURL(file);
+
+    this.preview.style.backgroundImage = `url('${this.blobUrl}')`;
+    this.helper.style.display = "none";
+    this.plus.style.display = "none";
+    this.removeBtn.style.display = "block";
+
+    if (this.onImageChange) this.onImageChange(this.imageId);
+  }
+
+  getImageDto() {
+    return {
+      originalName: this.imageOrigianlName,
+      objectKey: this.imageId,
+    };
+  }
+}

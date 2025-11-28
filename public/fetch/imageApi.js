a13765f62d5a7933a6eb4c5a0fadea08f97a69d9
jehyeok <wpgur07@gmail.com>
2025-11-28 14:43:49 +0900

    feat: image업로드 클래스 작성
    - 기존의 fetch가접근성이 낮다고 생각하여 별도 fetch 구성으로 >구현
    - 해당 핸들러는 프리사인드 url을 세팅하고, 파일을 세팅해 업로>드를 시도하도록 구현

---
+++ public/fetch/imageApi.js
@@ -0,0 +1,56 @@
+import { BASE_URL } from "/config.js";
+
+export class ImageHandler {
+  constructor() {
+    this.presignedPatchUrl = undefined;
+    this.objectKey = undefined;
+    this.file = undefined;
+  }
+
+  async setUrl() {
+    const res = await fetch(BASE_URL + "/images/url");
+    if (!res.ok) {
+      console.error("프리사인 URL 발급 실패");
+      return;
+    }
+
+    const data = await res.json();
+
+    this.presignedPatchUrl = data.data.presignedPatchUrl;
+    this.objectKey = data.data.objectKey;
+    return this.objectKey;
+  }
+
+  setFile(file) {
+    this.file = file;
+  }
+
+  async push() {
+    if (!this.file) {
+      console.error("파일이 등록되어 있지 않아 이미지 저장에 실패했습니다.");
+      return;
+    }
+    if (!this.presignedPatchUrl) {
+      console.error("프리사인 URL이 없습니다. 먼저 setUrl()을 호출하세요.");
+      return;
+    }
+
+    try {
+      const res = await fetch(this.presignedPatchUrl, {
+        method: "PUT",
+        headers: {
+          "Content-Type": this.file.type,
+        },
+        body: this.file,
+      });
+
+      if (res.ok) {
+        console.log("파일 업로드 성공");
+      } else {
+        console.error("파일 업로드 실패:", res.status, res.statusText);
+      }
+    } catch (err) {
+      console.error("업로드 중 오류:", err);
+    }
+  }
+}
